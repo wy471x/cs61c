d31467fd4a86e642a89145c6c9440a66734eeed2
wy471x <wy471x@gmail.com>
2022-12-20 23:57:11 +0800

    cs61c su20 lab01 finishing.

--- labs/lab01/no_segfault_ex.c
+++ labs/lab01/no_segfault_ex.c
@@ -2,7 +2,8 @@
 int main() {
     int a[5] = {1, 2, 3, 4, 5};
     unsigned total = 0;
-    for (int j = 0; j < sizeof(a); j++) {
+    printf("array len: %ld\n", sizeof(a) / sizeof(int));
+    for (int j = 0; j < sizeof(a) / sizeof(int); j++) {
         total += a[j];
     }
     printf("sum of array is %d\n", total);
--- labs/lab01/segfault_ex.c
+++ labs/lab01/segfault_ex.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
 int main() {
     int a[20];
-    for (int i = 0; ; i++) {
+    for (int i = 0; i < sizeof(a) / sizeof(int); i++) {
         a[i] = i;
+        printf("%d ", a[i]);
     }
+    printf("\n");
 }
