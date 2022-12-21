8b78cdf4ed0f8f668b2fbbf4a6ba48876c82d4c2
wy471x <wy471x@gmail.com>
2022-12-21 23:33:33 +0800

    cs61c su20 lab01 finished.

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -3,5 +3,13 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *slow = head, *fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow) {
+            return 1;
+        }
+    }
     return 0;
 }
