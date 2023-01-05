10884239bf6d85d858b7f4401f0e0871529ea80b
wy471x <wy471x@gmail.com>
2023-01-05 23:43:39 +0800

    cs61c su20 lab02 finishing.

--- labs/lab02/bit_ops.c
+++ labs/lab02/bit_ops.c
@@ -17,11 +17,13 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    *x = (*x & (~(1 << n))) | (v << n);
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    *x = *x ^ 1 << n;
 }
 
--- labs/lab02/test.c
+++ labs/lab02/test.c
@@ -4,11 +4,29 @@
 
 void test1(int x, int n);
 
+void test2(int x, int n, int v);
+
+void test3(int x, int n);
+
 int main(int argc, char **argv) {
-    test1(atoi(argv[1]), atoi(argv[2]));
+//    test1(atoi(argv[1]), atoi(argv[2]));
+//    test2(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+    test3(atoi(argv[1]), atoi(argv[2]));
     return 0;
 }
 
 void test1(int x, int n) {
     printf("x = %d n = %d  result = %d\n", x, n, get_bit(x, n));
+}
+
+void test2(int x, int n, int v) {
+    printf("x = %d ", x);
+    set_bit(&x, n, v);
+    printf("n = %d v = %d result = %d\n", n, v, x);
+}
+
+void test3(int x, int n) {
+    printf("x = %d ", x);
+    flip_bit(&x, n);
+    printf("n = %d result = %d\n", n, x);
 }
