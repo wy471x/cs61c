cb53abec717f5e9b7ae851d23aaf25f26dcbffc9
wy471x <wy471x@gmail.com>
2023-01-07 00:08:45 +0800

    cs61c su20 lab02 finishing.

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -3,8 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lfsr.h"
+#include "bit_ops.h"
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    int zero = get_bit(*reg, 0);
+    int two = get_bit(*reg, 2);
+    int three = get_bit(*reg, 3);
+    int five = get_bit(*reg, 5);
+    int target = zero ^ two ^ three ^ five;
+    *reg = *reg >> 1;
+    set_bit(reg, 15, target);
 }
 
