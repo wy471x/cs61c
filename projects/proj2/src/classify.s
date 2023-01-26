579a2d1c2ed1fb868ebd6debba2a9572f0a5db0b
wy471x <wy471x@gmail.com>
2023-01-26 23:03:24 +0800

    cs61c su20 proj2 finishing.

--- projects/proj2/src/classify.s
+++ projects/proj2/src/classify.s
@@ -41,7 +41,7 @@ classify:
     sw s8, 36(sp)
     sw s9, 40(sp)
     sw s10, 44(sp)
-    sw s11. 48(sp)
+    sw s11, 48(sp)
 
     # check correct number of command line args
     li t0, 5
@@ -100,102 +100,62 @@ classify:
     # 1. LINEAR LAYER:    m0 * input
     # 2. NONLINEAR LAYER: ReLU(m0 * input)
     # 3. LINEAR LAYER:    m1 * ReLU(m0 * input)
-    lw a1, 0(s3)
-        lw a2, 4(s3)
 
-        mv a3, s10
-        lw a4, 0(s9)
-        lw a5, 4(s9)
+    # LINEAR LAYER: m0 * input
+    lw a1, 0(s3) # load rows
+    lw a2, 4(s3) # load cols
 
-        mul t0, a1, a5
-        slli a0, t0, 2
-        addi sp, sp, -20
-        sw a1, 0(sp)
-        sw a2, 4(sp)
-        sw a3, 8(sp)
-        sw a4, 12(sp)
-        sw a5, 16(sp)
-        jal malloc
-        lw a5, 16(sp)
-        lw a4, 12(sp)
-        lw a3, 8(sp)
-        lw a2, 4(sp)
-        lw a1, 0(sp)
-        addi sp, sp, 20
+    mv a3, s10
+    lw a4, 0(s9)
+    lw a5, 4(s9)
 
-        beq a0, x0, malloc_error
-        mv s8, a0
+    # size of the m0 * input matrix
+    mul t0, a1, a5
+    # create m0 * input matrix
+    slli a0, t0, 2
+    addi sp, sp, -20
+    sw a1, 0(sp)
+    sw a2, 4(sp)
+    sw a3, 8(sp)
+    sw a4, 12(sp)
+    sw a5, 16(sp)
+    jal malloc
+    lw a5, 16(sp)
+    lw a4, 12(sp)
+    lw a3, 8(sp)
+    lw a2, 4(sp)
+    lw a1, 0(sp)
+    addi sp, sp, 20
 
-        mv a6, a0
-        mv a0, s4
+    beq a0, x0, malloc_error
+    mv s8, a0 # free malloc later
 
-        addi sp, sp, -4
-        sw a6, 0(sp)
-        jal matmul
-        lw a6, 0(sp)
-        addi sp, sp, 4
+    mv a6, a0
+    mv a0, s4 # load m0 matrix pointer
 
-        mv t0, a6
-        lw t1, 0(s3)
-        lw t2, 4(s9)
+    addi sp, sp, -4
+    sw a6, 0(sp)
+    jal matmul
+    lw a6, 0(sp)
+    addi sp, sp, 4
 
-        mv a0, t0
-        mul a1, t1, t2
+    mv t0, a6
+    lw t1, 0(s3)
+    lw t2, 4(s9)
 
-        addi sp, sp, -12
-        sw t0, 0(sp)
-        sw t1, 4(sp)
-        sw t2, 8(sp)
-        jal relu
-        lw t2, 8(sp)
-        lw t1, 4(sp)
-        lw t0, 0(sp)
-        addi sp, sp , 12
+    # NONLINEAR LAYER: ReLU(m0 * input)
+    mv a0, t0
+    mul a1, t1, t2
 
-        lw a1, 0(s6)
-        lw a2, 4(s6)
-
-        mv a3, t0
-        mv a4, t1
-        mv a5, t2
-
-        mul t1, a1, a5
-        slli a0, t1, 2
-
-        addi sp, sp, -28
-        sw t0, 0(sp)
-        sw t1, 4(sp)
-        sw a1, 8(sp)
-        sw a2, 12(sp)
-        sw a3, 16(sp)
-        sw a4, 20(sp)
-        sw a5, 24(sp)
-        jal malloc
-        lw a5, 24(sp)
-        lw a4, 20(sp)
-        lw a3, 16(sp)
-        lw a2, 12(sp)
-        lw a1, 8(sp)
-        lw t1, 4(sp)
-        lw t0, 0(sp)
-        addi sp, sp, 28
-
-        beq a0, x0, malloc_error
-        mv s0, a0
-        mv a6, a0
-        mv a0, s7
-
-        addi sp, sp, -16
-        sw t1, 0(sp)
-        sw a1, 4(sp)
-        sw a5, 8(sp)
-        sw a6, 12(sp)
-        jal matmul
-        lw a6, 12(sp)
-        lw a5, 8(sp)
-        lw a1, 4(sp)
-        lw t1, 0(sp)
-        addi sp, sp, 16
+    addi sp, sp, -12
+    sw t0, 0(sp)
+    sw t1, 4(sp)
+    sw t2, 8(sp)
+    jal relu
+    lw t2, 8(sp)
+    lw t1, 4(sp)
+    lw t0, 0(sp)
+    addi sp, sp , 12
 
     # =====================================
     # WRITE OUTPUT
@@ -205,7 +165,7 @@ classify:
     lw a2, 4(s6) # load m1 # of cols
 
     mv a3, t0 # load ReLU(m0 * input) matrix pointer
-    mv a4, t1   # load # of rows
+    mv a4, t1 # load # of rows
     mv a5, t2 # load # of cols
 
     mul t1, a1, a5 # size of the m1 * ReLU(m0 * input)
@@ -247,6 +207,9 @@ classify:
     lw t1, 0(sp)
     addi sp, sp, 16
 
+    # ====================================
+    # WRITE FILE
+    # ====================================
     lw a0, 16(s1)
     mv a2, a1
     mv a3, a5
@@ -274,32 +237,32 @@ classify:
     mv s11, a0
 
     # Print classification
-    bne s2, x0 done
-    mv a1 s11
+    bne s2, x0, done
+    mv a1, s11
     jal print_int
 
     # Print newline afterwards for clarity
-    li a1 '\n'
+    li a1, '\n'
     jal print_char
 
     # =====================================
     # FREE MALLOC
     # =====================================
-    mv a0 s3
+    mv a0, s3
     jal free
-    mv a0 s6
+    mv a0, s6
     jal free
-    mv a0 s9
+    mv a0, s9
     jal free
-    mv a0 s4
+    mv a0, s4
     jal free
-    mv a0 s7
+    mv a0, s7
     jal free
-    mv a0 s8
+    mv a0, s8
     jal free
-    mv a0 s0
+    mv a0, s0
     jal free
-    mv a0 s10
+    mv a0, s10
     jal free
 
 done:
